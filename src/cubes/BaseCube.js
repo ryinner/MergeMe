b4cb0076e6b2499f10fd52d41a8a66e62467ab22
ryinner <vinograd18062002@gmail.com>
2022-03-30 13:15:54 +0500

    random color

--- src/cubes/BaseCube.js
+++ src/cubes/BaseCube.js
@@ -6,7 +6,17 @@ export class BaseCube {
     render() {
         let cube = document.createElement('div')
         cube.classList.add('cube')
+        cube.style.background = this.randomColor()
 
         this.app.append(cube)
+    }
+
+    randomColor() {
+        const colors = [
+            '#fff',
+            '#000',
+        ]
+
+        return colors[Math.floor(Math.random() * colors.length)]
     }
 }
