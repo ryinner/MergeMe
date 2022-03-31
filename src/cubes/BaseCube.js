202e6bf8d152dbd4308f9d788a064da59c8d47af
ryinner <vinograd18062002@gmail.com>
2022-03-31 11:56:01 +0500

    calculateswipe

--- src/cubes/BaseCube.js
+++ src/cubes/BaseCube.js
@@ -9,6 +9,8 @@ export class BaseCube {
         this.cube.style.background = this.randomColor()
 
         this.app.append(this.cube)
+
+        this.swipe()
     }
 
     randomColor() {
@@ -20,10 +22,47 @@ export class BaseCube {
         return colors[Math.floor(Math.random() * colors.length)]
     }
 
-    // swipe() {
-    //     this.cube.addEventListener('mousemove', (event) => {
-    //         console.log(event.movementX)
-    //         console.log(event.movementY)
-    //     })
-    // }
+    swipe() {
+        let touch = {}
+        this.cube.addEventListener('touchstart', (event) => {
+            touch = { 
+                x: event.changedTouches[0].clientX,
+                y: event.changedTouches[0].clientY,
+            }
+        })
+
+        this.cube.addEventListener('touchmove', (event) => {
+            let touchMove = { 
+                x: event.changedTouches[0].clientX,
+                y: event.changedTouches[0].clientY,
+            }
+
+            const calcX = touch.x - touchMove.x
+            const calcY = touch.y - touchMove.y
+
+            this.calculateChangeOfPosition(calcX, calcY)
+        })
+    }
+
+    calculateChangeOfPosition(calcX, calcY) {
+        if (Math.abs(calcX) > Math.abs(calcY)) {
+            // r-l
+            if (calcX > 0) {
+                console.log('r');
+            } else {
+                console.log('l');
+            }
+        } else {
+            // t-b
+            if(calcY > 0) {
+                console.log('t');
+            } else {
+                console.log('b');
+            }
+        }
+    }
+
+    transform() {
+        
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { BaseCube } from "./cubes/BaseCube.js"
 
 const APP = document.querySelector('#app')
-const CANVAS = document.querySelector('#canvas')
 
 const cube = new BaseCube(APP)
-const cube2 = new BaseCube(CANVAS)
+
 // 144 куба максимум
 let i = 0
 while (i < 144) {
